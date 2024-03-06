efa22703addc226501ec46bd8b5111c049193318
Bruno Borges <brunoborges2001@gmail.com>
2024-03-06 18:57:10 -0300

    fix: import typo

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignIn() {
   const { data: session, status } = useSession();
